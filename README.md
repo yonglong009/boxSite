2193c2621f67767acb61644fed7f7c0382b671d1
yonglong009 <yonglong009@yeah.net>
2015-10-23 16:58:55 +0800

    add hello

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # boxSite mayl
+on dev
--- boxSite/urls.py
+++ boxSite/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from firstApp import views
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^hello/$', views.hello, name='hello'),
 ]
